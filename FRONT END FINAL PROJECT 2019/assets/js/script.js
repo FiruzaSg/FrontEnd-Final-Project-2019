f91e54e6e0b72555853328bb8a7e49665c099fb2
FiruzaSg <56362392+FiruzaSg@users.noreply.github.com>
2019-12-10 17:11:18 +0400

    Creating sliders

--- FRONT END FINAL PROJECT 2019/assets/js/script.js
+++ FRONT END FINAL PROJECT 2019/assets/js/script.js
@@ -1,8 +1,55 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    
-    $(".alertbox .btnclose").click(function(){
+
+    $(".alertbox .btnclose").click(function () {
         $(".alertbox").addClass("d-none");
     });
+
+
+    $(".navbar .navbar-nav .nav-item").hover(function () {
+
+        if ($(this).find(".dropdown-menu").hasClass("show")) {
+            $(this).find(".dropdown-menu").removeClass("show")
+        } else {
+            $(this).find(".dropdown-menu").addClass("show")
+        }
+    })
+
+
+    // $('.popular-item').slick({
+    //     dots: true,
+    //     infinite: false,
+    //     speed: 300,
+    //     slidesToShow: 4,
+    //     slidesToScroll: 4,
+    //     responsive: [
+    //       {
+    //         breakpoint: 1024,
+    //         settings: {
+    //           slidesToShow: 3,
+    //           slidesToScroll: 3,
+    //           infinite: true,
+    //           dots: true
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 600,
+    //         settings: {
+    //           slidesToShow: 2,
+    //           slidesToScroll: 2
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 480,
+    //         settings: {
+    //           slidesToShow: 1,
+    //           slidesToScroll: 1
+    //         }
+    //       }
+    //     ]
+    //   });
+
+
+
 });
 
