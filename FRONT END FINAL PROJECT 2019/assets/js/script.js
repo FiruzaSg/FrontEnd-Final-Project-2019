aacb5dff85b056ff9472c8182afa7e7a9f460cf7
FiruzaSg <56362392+FiruzaSg@users.noreply.github.com>
2019-12-10 21:39:47 +0400

    Created "about us" section

--- FRONT END FINAL PROJECT 2019/assets/js/script.js
+++ FRONT END FINAL PROJECT 2019/assets/js/script.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
 
 
     $(".navbar .navbar-nav .nav-item").hover(function () {
-
         if ($(this).find(".dropdown-menu").hasClass("show")) {
             $(this).find(".dropdown-menu").removeClass("show")
         } else {
@@ -16,38 +15,6 @@ $(document).ready(function () {
     })
 
 
-    // $('.popular-item').slick({
-    //     dots: true,
-    //     infinite: false,
-    //     speed: 300,
-    //     slidesToShow: 4,
-    //     slidesToScroll: 4,
-    //     responsive: [
-    //       {
-    //         breakpoint: 1024,
-    //         settings: {
-    //           slidesToShow: 3,
-    //           slidesToScroll: 3,
-    //           infinite: true,
-    //           dots: true
-    //         }
-    //       },
-    //       {
-    //         breakpoint: 600,
-    //         settings: {
-    //           slidesToShow: 2,
-    //           slidesToScroll: 2
-    //         }
-    //       },
-    //       {
-    //         breakpoint: 480,
-    //         settings: {
-    //           slidesToShow: 1,
-    //           slidesToScroll: 1
-    //         }
-    //       }
-    //     ]
-    //   });
 
 
 
