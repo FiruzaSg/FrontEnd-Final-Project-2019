76fffeaaf14b66a2f2b74a6a3b73b7cff58e49aa
FiruzaSg <56362392+FiruzaSg@users.noreply.github.com>
2019-12-12 21:29:44 +0400

    Bottom counter completed

--- FRONT END FINAL PROJECT 2019/assets/js/script.js
+++ FRONT END FINAL PROJECT 2019/assets/js/script.js
@@ -1,43 +1,44 @@
-$(document).ready(function () {
-
-
-  $(".alertbox .btnclose").click(function () {
+$(document).ready(function() {
+  $(".alertbox .btnclose").click(function() {
     $(".alertbox").addClass("d-none");
   });
 
-
-  $(".navbar .navbar-nav .nav-item").hover(function () {
-    if ($(this).find(".dropdown-menu").hasClass("show")) {
-      $(this).find(".dropdown-menu").removeClass("show")
+  $(".navbar .navbar-nav .nav-item").hover(function() {
+    if (
+      $(this)
+        .find(".dropdown-menu")
+        .hasClass("show")
+    ) {
+      $(this)
+        .find(".dropdown-menu")
+        .removeClass("show");
     } else {
-      $(this).find(".dropdown-menu").addClass("show")
+      $(this)
+        .find(".dropdown-menu")
+        .addClass("show");
     }
-  })
+  });
 
-  if ($(".companies-slider").length){
-      $('.companies-slider').slick({
-        // infinite: true,
-        slidesToShow: 6,
-        slidesToScroll: 1
+  if ($(".companies-slider").length) {
+    $(".companies-slider").slick({
+      // infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 1
     });
   }
- 
 
+  if ($("#bottom-counter").length) {
+    $("#bottom-counter .value").counterUp({
+      delay: 10,
+      time: 2000
+    });
+  }
 
-if ($("#bottom-counter").length){
-  $('#bottom-counter .value').counterUp({
-    delay: 10,
-    time: 2000
-  });
-}
- 
-if ($("#about-us .counter").length){
-  
-  $('#about-us .counter .value').counterUp({
-    delay: 10,
-    time: 2000
-  });
-}
-    
+  if ($("#about-us .counter").length) {
+    $("#about-us .counter .value").counterUp({
+      delay: 10,
+      time: 2000
+    });
+  }
+
 });
-
